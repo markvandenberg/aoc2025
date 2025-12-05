2d0716bb754aee328d69fff2848b3d8dcde184cb
Mark van den Berg <mark@boxfish.nl>
2025-12-05 16:17:02 +0100

    Day 5 part 1

--- day05/solution.py
+++ day05/solution.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Advent of Code 2025 - Day X
+Advent of Code 2025 - Day 5: Cafeteria
 """
 
 def read_input(filename='input.txt'):
@@ -10,28 +10,55 @@ def read_input(filename='input.txt'):
 
 
 def parse_input(data):
-    """Parse the input data."""
-    lines = data.split('\n')
-    return lines
+    """Parse the input into fresh ranges and available IDs."""
+    parts = data.split('\n\n')
+    
+    # Parse fresh ranges
+    fresh_ranges = []
+    for line in parts[0].split('\n'):
+        start, end = map(int, line.split('-'))
+        fresh_ranges.append((start, end))
+    
+    # Parse available IDs
+    available_ids = []
+    for line in parts[1].split('\n'):
+        available_ids.append(int(line))
+    
+    return fresh_ranges, available_ids
+
+
+def is_fresh(ingredient_id, fresh_ranges):
+    """Check if an ingredient ID is fresh (falls into any range)."""
+    for start, end in fresh_ranges:
+        if start <= ingredient_id <= end:
+            return True
+    return False
 
 
 def part1(data):
-    """Solve part 1."""
-    parsed = parse_input(data)
-    # TODO: Implement solution
-    return None
+    """Count how many available ingredient IDs are fresh."""
+    fresh_ranges, available_ids = parse_input(data)
+    
+    fresh_count = 0
+    for ingredient_id in available_ids:
+        if is_fresh(ingredient_id, fresh_ranges):
+            fresh_count += 1
+    
+    return fresh_count
 
 
 def part2(data):
     """Solve part 2."""
-    parsed = parse_input(data)
-    # TODO: Implement solution
+    fresh_ranges, available_ids = parse_input(data)
+    # TODO: Implement solution when part 2 is unlocked
     return None
 
 
 def main():
-    # Read input
-    data = read_input()
+    import sys
+    # Read input (use command line argument if provided, otherwise default to input.txt)
+    filename = sys.argv[1] if len(sys.argv) > 1 else 'input.txt'
+    data = read_input(filename)
     
     # Solve parts
     result1 = part1(data)
